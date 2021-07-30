39ff4b8978d9275d7e28a28a507c7588084bbbb4
= <56016006+thebush99@users.noreply.github.com>
2021-07-30 21:28:17 +0700

    first file

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {QueryClient, QueryClientProvider, useQuery} from 'react-query'
+import axios from 'axios'
+
+function App() {
+
+  return (
+      <div className="App">
+        <Number />
+        <Example />
+      </div>
+  );
+}
+
+export default App;
+
+function useTodo(){
+ return useQuery('todos',() =>
+   axios.get('https://jsonplaceholder.typicode.com/users').then(res => res.data)
+  )
+}
+function Number(){
+  const number = useTodo();
+  return <div> Your name length are {number?.data?.length} names</div>
+}
+function Example(){
+  const result = useTodo()  
+  console.log(result)
+
+  if(result.isFetching){ return "Loading..."}
+
+  if(result.isError){ return "Error.." }
+
+  return (
+    <div>
+      {result?.data?.map(item => 
+          <div>{item.name}</div> 
+      )}
+      {result.isFetching ? "Updating" : null}
+    </div>
+
+  );
+}
